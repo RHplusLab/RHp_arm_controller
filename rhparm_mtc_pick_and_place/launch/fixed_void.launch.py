f9d99b96fe95188c4a96b9db03ab8dc4c1f22530
Chanhui.Robot <chanhui.robot@yonsei.ac.kr>
2025-03-14 12:48:57 +0900

    [Create] create new repository : RHp_arm_controller

---
+++ rhparm_mtc_pick_and_place/launch/fixed_void.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("rhparm").to_dict()
+
+    # MTC Demo node
+    pick_place_demo = Node(
+        package="rhparm_mtc_pick_and_place",
+        executable="fixed_void",
+        output="screen",
+        parameters=[
+            moveit_config,
+        ],
+    )
+
+    return LaunchDescription([pick_place_demo])
